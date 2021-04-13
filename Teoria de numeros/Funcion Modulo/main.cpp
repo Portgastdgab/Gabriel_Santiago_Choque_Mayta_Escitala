25cf6294826f4729db31ad31735c774bd87781a9
GoldGab <gabriel.choque@ucsp.edu.pe>
2021-04-13 14:54:49 -0500

    corregi funcion modulo+

--- Teoria de numeros/Funcion Modulo/main.cpp
+++ Teoria de numeros/Funcion Modulo/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int division(int a, int n){
+int modulo(int a, int n){
    int r = a-n*(a/n); //Halla el residuo sin necesidad de usar modulo "%"
    r = r+(r<0)*n; //Vuelvo a hallar el residuo por si r antes era negativo
    return r;
 }
 
 int main() {
-    int residuo = division(-255,11);
+    int residuo = modulo(-255,11);
     cout<<residuo;
 }
 
