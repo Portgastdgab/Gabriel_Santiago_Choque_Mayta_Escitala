fd03edc8d5adf6c54fab1ada72ecd39d310ba5d5
GoldGab <gabriel.choque@ucsp.edu.pe>
2021-04-15 20:02:25 -0500

    18.7 terminado

---
+++ Manejo de string/18.7/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class Cesar{
+    string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+    string palabra;
+    int clave = 13;
+
+    int modulo(int a, int n){
+        int r = a-n*(a/n);
+        r = r+(r<0)*n;
+        return r;
+    }
+
+public:
+    string Cifrado(string mensaje){
+        for (int i = 0; i < mensaje.size(); ++i) {
+            palabra.push_back(alfabeto[modulo(alfabeto.find(mensaje[i])+clave, 26)]);
+        }
+        return palabra;
+    }
+    string Descifrado(string mensaje){
+        for (int i = 0; i < mensaje.size(); ++i) {
+            palabra.push_back(alfabeto[modulo(alfabeto.find(mensaje[i])-clave, 26)]);
+        }
+        return palabra;
+    }
+};
+
+int main() {
+    string mensaje = "hola";
+    mensaje = Cesar().Cifrado(mensaje);
+    cout<<"Cifrado: "<<mensaje<<endl;
+    mensaje = Cesar().Descifrado(mensaje);
+    cout<<"Descifrado: "<<mensaje;
+}
