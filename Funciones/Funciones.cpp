984bfb48418f72010e459ad41e457fdbb091435b
GoldGab <gabriel.choque@ucsp.edu.pe>
2021-05-25 13:56:28 -0500

    Criba de eratostenes

--- Funciones/Funciones.cpp
+++ Funciones/Funciones.cpp
@@ -80,3 +80,34 @@ int random(int begin, int end) {
     srand(time(NULL));
     return begin + rand() % (end - begin);
 }
+
+vector<int> criba_eratostenes(int limit)
+{
+    vector<int> criba;
+    int prime_found;  // primos que se vayan encontrando
+
+    // llenando el vector desde 2 hasta limit
+    for (int i = 2; i <= limit; i++)
+        criba.push_back(i);
+
+    if (limit == 2 || limit == 3)
+        return criba;
+
+    // iteradores para la seleccion
+    vector<int>::iterator it = criba.begin();
+    vector<int>::iterator it2;
+    prime_found = *it;  // El primer primo es 2
+
+    while (prime_found*prime_found < limit)
+    {
+        it2 = it + 1;
+
+        for(; it2 <= criba.end(); it2++) //Borra multiplos de los primos hallados
+            if(*it2 % prime_found == 0)
+                criba.erase(it2);
+        it++;
+        prime_found = *it;
+    }
+
+    return criba;
+}
--- Funciones/Funciones.h
+++ Funciones/Funciones.h
@@ -1,5 +1,6 @@
-#include<stdlib.h>
-#include<time.h>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
 
 int module(int, int );
 int recursive_Euclid(int, int );
@@ -8,3 +9,4 @@ int Euclid_extended(int, int );
 int inverse(int, int );
 int modular_exponentiation(int, int, int );
 int random(int begin = 0, int ind = 100);
+vector<int> criba_eratostenes(int);
